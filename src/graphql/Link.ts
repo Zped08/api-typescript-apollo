aa7ffc7819eff386f2ebff7c1ab18ac04829fa96
Jair Flores <jairfloresa0802@gmail.com>
2023-06-12 13:35:10 +0200

    actualizando Link

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -2,62 +2,67 @@ import { objectType, extendType, nonNull, stringArg } from "nexus";
 import { NexusGenObjects } from "../../nexus-typegen";
 
 export const Link = objectType({
-    name: "Link", // 1 
-    definition(t) {  // 2
-        t.nonNull.int("id"); // 3 
-        t.nonNull.string("description"); // 4
-        t.nonNull.string("url"); // 5 
-    },
+  name: "Link", // 1
+  definition(t) {
+    // 2
+    t.nonNull.int("id"); // 3
+    t.nonNull.string("description"); // 4
+    t.nonNull.string("url"); // 5
+  },
 });
 
-
-let links: NexusGenObjects["Link"][]= [   // 1
-    {
-        id: 1,
-        url: "www.howtographql.com",
-        description: "Fullstack tutorial for GraphQL",
-    },
-    {
-        id: 2,
-        url: "graphql.org",
-        description: "GraphQL official website",
-    },
+let links: NexusGenObjects["Link"][] = [
+  // 1
+  {
+    id: 1,
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+  {
+    id: 2,
+    url: "graphql.org",
+    description: "GraphQL official website",
+  },
 ];
 
-export const LinkQuery = extendType({  // 2
-    type: "Query",
-    definition(t) {
-        t.nonNull.list.nonNull.field("feed", {   // 3
-            type: "Link",
-            resolve(parent, args, context, info) {    // 4
-                return links;
-            },
-        });
-    },
+export const LinkQuery = extendType({
+  // 2
+  type: "Query",
+  definition(t) {
+    t.nonNull.list.nonNull.field("feed", {
+      // 3
+      type: "Link",
+      resolve(parent, args, context, info) {
+        // 4
+        return context.prisma.link.findMany(); // 1
+      },
+    });
+  },
 });
 
-export const LinkMutation = extendType({  // 1
-    type: "Mutation",    
-    definition(t) {
-        t.nonNull.field("post", {  // 2
-            type: "Link",  
-            args: {   // 3
-                description: nonNull(stringArg()),
-                url: nonNull(stringArg()),
-            },
-            
-            resolve(parent, args, context) {    
-                const { description, url } = args;  // 4
-                
-                let idCount = links.length + 1;  // 5
-                const link = {
-                    id: idCount,
-                    description: description,
-                    url: url,
-                };
-                links.push(link);
-                return link;
-            },
+export const LinkMutation = extendType({
+  // 1
+  type: "Mutation",
+  definition(t) {
+    t.nonNull.field("post", {
+      // 2
+      type: "Link",
+      args: {
+        // 3
+        description: nonNull(stringArg()),
+        url: nonNull(stringArg()),
+      },
+
+      resolve(parent, args, context) {
+        const newLink = context.prisma.link.create({
+          // 2
+          data: {
+            description: args.description,
+            url: args.url,
+          },
         });
-    },
+        return newLink;
+      },
+    });
+  },
 });
