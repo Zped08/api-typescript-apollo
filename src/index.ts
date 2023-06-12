97adf5786e0b3db6731ebe052817d7834718e256
Jair Flores <jairfloresa0802@gmail.com>
2023-06-12 20:03:14 +0200

    update de index.ts and add script in package.json

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import { ApolloServer } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import { context } from "./context";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
 
 // 1
 import { schema } from "./schema";
 export const server = new ApolloServer({
   schema,
   context,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  //plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  introspection: true, // 1
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;   
 // 2
 server.listen({ port }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
